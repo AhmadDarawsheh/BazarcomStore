e226b87216f9a73c28e1fa78f043078a2cfad662
Ahmad Darawsheh <hmod00asad@gmail.com>
2023-12-31 18:45:41 +0200

    using redis to deal with cache, updated and implemented some functions neccessry for cache

--- frontserver/index.js
+++ frontserver/index.js
@@ -3,6 +3,11 @@ const program = new Command();
 const axios = require("axios");
 const readline = require("readline");
 const redis = require("redis");
+const express = require("express");
+const { resolve } = require("path");
+
+const app = express();
+app.use(express.json());
 
 const client = redis.createClient({
   host: "localhost",
@@ -30,11 +35,15 @@ const toggleCatalogServer = () => {
 };
 
 const toggleOrderServer = () => {
-  const server = `http://localhost:400${catalogServer}`;
+  const server = `http://localhost:400${orderServer}`;
   orderServer = (orderServer % 2) + 1;
   return server;
 };
 
+const invalidateCache = (key) => {
+  client.del(key);
+  console.log("deleted")
+};
 const mainmenu = () => {
   r1.question('Enter a comamand (Type "exit" to quit."):', (input) => {
     const [command, bookId] = input.split(" ");
@@ -48,6 +57,18 @@ const mainmenu = () => {
     }
   });
 };
+
+app.post("/invalidateCache", async (req, res) => {
+  try {
+    const { key } = req.body;
+    const invalid = `catalog_${key}`
+    await invalidateCache(invalid);
+    console.log("Cache Invalidated");
+  } catch (err) {
+    console.error("Error invalidating cache", err);
+    res.json({ message: "Error invalidating cache" });
+  }
+});
 
 program
   .name("DOSMicroservices")
@@ -66,35 +87,50 @@ program
         return;
       }
       if (cachedData) {
-        console.log("Cache Hit: ", JSON.parse(cachedData));
+        console.log("\nCache Hit: ", JSON.parse(cachedData));
         return;
       }
 
-
       console.log(catalogServer);
-    const serverUrl = toggleCatalogServer();
+      const serverUrl = toggleCatalogServer();
 
-    console.log("Fetching data from the server: ", serverUrl);
-    axios
-      .get(`${serverUrl}/Bazarcom/searchim/${bookId}`)
-      .then((response) => {
-        console.log("Cache miss. Retrived data from server ", response.data);
-        client.setex(cacheKey, 3600, JSON.stringify(response.data));
-        return;
-      })
-      .catch((err) => {
-        console.error("Error retrieving data", err.message);
-      });
+      console.log("Fetching data from the server: ", serverUrl);
+      axios
+        .get(`${serverUrl}/Bazarcom/searchim/${bookId}`)
+        .then((response) => {
+          console.log(
+            "\nCache miss. Retrived data from server ",
+            response.data
+          );
+          client.setex(cacheKey, 3600, JSON.stringify(response.data));
+          return;
+        })
+        .catch((err) => {
+          console.error("Error retrieving data", err.message);
+        });
     });
-    
   });
 
 program
-  .command("list")
-  .alias("l")
+  .command("purchase <bookId>")
+  .alias("p")
   .description("List all the books.")
-  .action(() => {
-    console.log("Listing the books");
+  .action((bookId) => {
+    const serverUrl = toggleOrderServer();
+    console.log("Fetching data from the server: ", serverUrl);
+
+    axios
+      .get(`${serverUrl}/purchase/${bookId}`)
+      .then((response) => {
+        console.log("\nBook purchased successfully!");
+      })
+      .catch((err) => {
+        console.error("Error purchasing the book", err.message);
+      });
   });
 
+app.listen(5000, () => {
+  console.log("\n");
+  console.log("Frontend server is running...");
+});
 mainmenu();
